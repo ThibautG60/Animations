60e29bdf3ed6cff7f4beac682d8d125a0cebac64
ThibautG60 <hatomalis@gmail.com>
2025-01-31 18:30:53 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let navbar = document.querySelector('header');
+let logo = document.getElementById('logo');
+let mainPos = document.querySelector('main').children;
+
+window.addEventListener('scroll', function () {
+    if (mainPos[0].getBoundingClientRect().y <= 0 && mainPos[0].getBoundingClientRect().y >= -50 || mainPos[1].getBoundingClientRect().y >= 0 && mainPos[1].getBoundingClientRect().y <= 50) {
+        navbar.style.backgroundColor = 'rgb(13, 47, 129)';
+        navbar.firstElementChild.style.color = 'white';
+        logo.style.fill = 'rgb(255, 231, 170)';
+    }
+
+    if (mainPos[1].getBoundingClientRect().y <= 0 && mainPos[1].getBoundingClientRect().y >= -50 || mainPos[2].getBoundingClientRect().y >= 0 && mainPos[2].getBoundingClientRect().y <= 50) {
+        navbar.style.backgroundColor = 'rgb(255, 231, 170)';
+        navbar.firstElementChild.style.color = 'black';
+        logo.style.fill = 'rgb(13, 47, 129)';
+    }
+
+    if (mainPos[2].getBoundingClientRect().y <= 0 && mainPos[2].getBoundingClientRect().y >= -50) {
+        navbar.style.backgroundColor = 'rgb(13, 47, 129)';
+        navbar.firstElementChild.style.color = 'white';
+        logo.style.fill = 'rgb(255, 231, 170)';
+    }
+});
